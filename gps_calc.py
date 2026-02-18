0fa7f2b91a84ee77186b29834f456b58f33a748e
Dervin Tian <dervint@umich.edu>
2026-02-17 21:03:19 -0500

    Started new gps calculations

--- gps_calc.py
+++ gps_calc.py
@@ -1,59 +1,91 @@
 import math
 import numpy as np
 
+def align_coordinates(gps_coord, theta):
+    new_tr_thingy = gps_coord[0] - h
+    omega = (math.pi) - ((math.pi / 2) + theta)
+    alpha = math.pi - ((math.pi / 2) + omega)
+    b = (h * math.sin(alpha) / math.sin(omega))
+    new_tr_other_thingy = gps_coord[1] + b
+    new_tr = (new_tr_thingy, new_tr_other_thingy)
+    return new_tr
+
 #need camera GPS position, camera altitude, camera orientation (roll, pitch, yaw), camera focal length & scaling factor
 
-#Step 1- convert pixel to camera ray
-f = 0 #physical focal length of camera (TODO: replace)
-s = 0 #pixel size scaling factor (TODO: replace)
-fxy = f/s
-iw = 0 #image width (TODO: replace)
-ih = 0 #image height (TODO: replace)
-cx = iw/2 #normalized camera x-coord
-cy = ih/2 #normalized camera y-coord
+# #Step 1- convert pixel to camera ray
+# f = 0 #physical focal length of camera (TODO: replace)
+# s = 0 #pixel size scaling factor (TODO: replace)
+# # fxy = f/s
+# iw = 0 #image width (TODO: replace)
+# ih = 0 #image height (TODO: replace)
+# cx = iw/2 #normalized camera x-coord
+# cy = ih/2 #normalized camera y-coord
 
-u = 0 #mine/pixel x-coordinate, center of bounding box (TODO: replace)
-v = 0 #mine/pixel y-coordinate, center of bounding box (TODO: replace)
-xc = (u -cx)/fxy #x-coord in camera
-yc = (v -cy)/fxy #y-coord in camera
+# u = 0 #mine/pixel x-coordinate, center of bounding box (TODO: replace)
+# v = 0 #mine/pixel y-coordinate, center of bounding box (TODO: replace)
+# # xc = (u -cx)/fxy #x-coord in camera
+# # yc = (v -cy)/fxy #y-coord in camera
 
-r = [xc, yc, 1] #ray in camera space
-
-
-#Step 2- rotate ray (tilt adjustment)
-roll = 0 #ϕ (TODO: replace)
-pitch = 0 #θ (TODO: replace)
-yaw = 0 #ψ (TODO: replace)
-
-Rx = []
-Rx.append([1, 0, 0])
-Rx.append([0, np.cos(roll), -1*np.sin(roll)])
-Rx.append([0, np.sin(roll), -1*np.cos(roll)])
-Ry = []
-Ry.append([1, 0, 0])
-Ry.append([0, np.cos(pitch), -1*np.sin(pitch)])
-Ry.append([0, np.sin(pitch), -1*np.cos(pitch)])
-Rz = []
-Rz.append([np.cos(yaw), -1*np.sin(yaw), 0])
-Rz.append([np.sin(yaw), -1*np.cos(yaw), 0])
-Rz.append([0, 0, 1])
-R = Rz*Ry*Rx
-r_world = R*r
+# # r = [xc, yc, 1] #ray in camera space
 
 
-#Step 3- intersect ray with ground
-H = 0 #camera height (TODO: replace)
-Xc = 0 #X-coord of camera irl (TODO: replace)
-Yc = 0 #Y-coord of camera irl (TODO: replace)
-X = Xc + (H/r_world[2])*r[0] #X-coord of mine irl
-Y = Yc + (H/r_world[2])*r[1] #Y-coord of mine irl
+# #Step 2- rotate ray (tilt adjustment)
+# roll = 0 #ϕ (TODO: replace)
+# pitch = 0 #θ (TODO: replace)
+# yaw = 0 #ψ (TODO: replace)
+
+# Rx = []
+# Rx.append([1, 0, 0])
+# Rx.append([0, np.cos(roll), -1*np.sin(roll)])
+# Rx.append([0, np.sin(roll), -1*np.cos(roll)])
+# Ry = []
+# Ry.append([1, 0, 0])
+# Ry.append([0, np.cos(pitch), -1*np.sin(pitch)])
+# Ry.append([0, np.sin(pitch), -1*np.cos(pitch)])
+# Rz = []
+# Rz.append([np.cos(yaw), -1*np.sin(yaw), 0])
+# Rz.append([np.sin(yaw), -1*np.cos(yaw), 0])
+# Rz.append([0, 0, 1])
+# R = Rz*Ry*Rx
+# r_world = R*r
 
 
-#Step 4- convert meters to latitude & longitude
-lat_cam = 0 #latitude of camera (TODO: replace)
-long_cam = 0 #longitude of camera (TODO: replace)
-lat = lat_cam + Y/111320
-long = long_cam + X/(111320*np.cos(roll))
+# #Step 3- intersect ray with ground
+# H = 0 #camera height (TODO: replace)
+# Xc = 0 #X-coord of camera irl (TODO: replace)
+# Yc = 0 #Y-coord of camera irl (TODO: replace)
+# X = Xc + (H/r_world[2])*r[0] #X-coord of mine irl
+# Y = Yc + (H/r_world[2])*r[1] #Y-coord of mine irl
+
+
+# #Step 4- convert meters to latitude & longitude
+# lat_cam = 0 #latitude of camera (TODO: replace)
+# long_cam = 0 #longitude of camera (TODO: replace)
+# lat = lat_cam + Y/111320
+# long = long_cam + X/(111320*np.cos(roll))
+
+field_tl = (42.411131, -83.498292)
+field_tr = (42.411169, -83.497703)
+field_br = (42.410189, -83.497575)
+field_bl = (42.410147, -83.498169)
+
+field_width = 49.15
+field_height = 109.82
+
+w = field_tr[1] - field_tl[1]
+h = field_tr[0] - field_tl[0]
+print(w, h)
+theta = math.atan(h/w)
+print(math.degrees(theta))
+
+aligned_tr = align_coordinates(field_tr, theta)
+print(aligned_tr)
+
+aligned_br = align_coordinates(field_br, theta)
+print(aligned_br)
+
+aligned_bl = align_coordinates(field_bl, theta)
+print(aligned_bl)
 
 
 
